375cde6afce66a256e4a90cdf6c4ab296ec97014
TienMinh25 <letienminh2512@gmail.com>
2023-11-28 21:18:27 +0700

    Initialize project

---
+++ app/src/main/java/com/tienminh/a5s_project_hand_on/MainActivity.java
@@ -0,0 +1,14 @@
+package com.tienminh.a5s_project_hand_on;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
